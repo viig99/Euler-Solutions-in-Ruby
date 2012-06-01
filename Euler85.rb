3094eefa82d19486d0fe5591d765f7c8561c3787
Arjun Variar <accio.arjun@gmail.com>
2012-06-01 11:21:52 +0530

    Problem 85

---
+++ Euler85.rb
@@ -0,0 +1,11 @@
+# Simple Solution
+
+max = x_end = y_end = 0
+limit = 2_000_000
+[*0...100].each do |x|
+	[*0...100].each do |y|
+		sum = (x**2+x)*(y**2+y)/4
+		max,x_end,y_end = [max,sum].max,x,y if (max < limit)			
+	end
+end
+p (x_end*y_end)
