7e2276e6f2ac09072e8fea9033fcb25e0dec0af0
Arjun Variar <accio.arjun@gmail.com>
2012-06-01 11:23:10 +0530

    Comments

--- Euler85.rb
+++ Euler85.rb
@@ -4,7 +4,7 @@ max = x_end = y_end = 0
 limit = 2_000_000
 [*0...100].each do |x|
 	[*0...100].each do |y|
-		sum = (x**2+x)*(y**2+y)/4
+		sum = (x**2+x)*(y**2+y)/4  # From Wikipedia
 		max,x_end,y_end = [max,sum].max,x,y if (max < limit)			
 	end
 end
